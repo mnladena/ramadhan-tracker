a6be142c769ea877417d17eafad9dace26d11a18
M Nur Lanta Adena <mnl.adena@gmail.com>
2026-02-24 17:41:55 +0700

    update icon

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "ramadhan-tracker-v1";
 const ASSETS_TO_CACHE = [
   "/",
   "/manifest.json",
-  "/icons/icon.svg",
+  "/icons/icon.webp",
 ];
 
 self.addEventListener("install", (event) => {
